cafce2dc1441e61ec0d4a083d06be5b5cc664ace
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2021-04-26 18:41:44 -0400

    Complete Stage 2 (Not just numbers)
    Upgrade the program to work with words and lines in addition to numbers.
    Program now accepts a command-line argument to control which type it reads and finds the largest/maximum.
    Wrote several new functions keeping everything as generic as possible throughout. Only has two or three spots where we have to re-duplicate code.

--- Main.java
+++ Main.java
@@ -1,25 +1,119 @@
 package sorting;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-    private static final HashMap<Long, Integer> numbers = new HashMap<>();
-    public static void main(final String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long max = Long.MIN_VALUE;
-        int count = 0;
 
-        while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            count++;
-            if (number > max) {
-                max = number;
-            }
-            numbers.put(number, numbers.getOrDefault(number, 0) + 1);
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(final String[] args) {
+        String type = parseArgs(args);
+        var input = readFromScanner(type);
+        var map = mapValues(input);
+        var largest = type.equals("long") ? findMaximum(map.keySet()) : findLongest(map.keySet());
+        String totalLine = "";
+        String countLine = "";
+
+        switch (type) {
+            case "long":
+                totalLine = "Total numbers: %d%n";
+                countLine = "The greatest number: %s (%d time(s), %3d%%).%n";
+                break;
+            case "word":
+                totalLine = "Total words: %d%n";
+                countLine = "The longest word: %s (%d time(s), %3d%%).%n";
+                break;
+            case "line":
+                totalLine = "Total lines: %d%n";
+                countLine = "The longest line:%n%s%n(%d time(s), %3d%%).%n";
+                break;
+            default:
+                break;
         }
 
-        System.out.printf("Total numbers: %d%n", count);
-        System.out.printf("The greatest number: %d (%d time(s)).%n",
-                max, numbers.get(max));
+        System.out.printf(totalLine, input.size());
+        System.out.printf(countLine, largest, map.get(largest), percentage(map.get(largest), input.size()));
+    }
+
+    private static String parseArgs(String[] args) {
+        String type = "word";
+
+        if (args.length != 0) {
+            if (args.length == 2 && args[0].equals("-dataType")) {
+                // change value only if we get one of the other types
+                switch (args[1]) {
+                    case "long":
+                        type = "long";
+                        break;
+                    case "line":
+                        type = "line";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return type;
+    }
+
+    private static int percentage(int occurrences, int total) {
+        return (int) (occurrences / (double) total * 100);
+    }
+
+    private static ArrayList<String> readFromScanner(String type) {
+        String regex = "\\s+";
+        int limit = 0;
+
+        if (type.equals("line")) {
+            regex = "";
+            limit = 1;
+        }
+
+        ArrayList<String> inputs = new ArrayList<>();
+
+        while (scanner.hasNext()) {
+            String input = scanner.nextLine();
+            Collections.addAll(inputs, input.split(regex, limit));
+        }
+
+        return inputs;
+    }
+
+    private static HashMap<String, Integer> mapValues(ArrayList<String> input) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (var in : input) {
+            hashMap.put(in, hashMap.getOrDefault(in, 0) + 1);
+        }
+        return hashMap;
+    }
+
+    private static String findLongest(Set<String> keys) {
+        String longest = "";
+        for (var in : keys) {
+            if (in.length() > longest.length() ||
+                    (in.length() == longest.length() && in.compareTo(longest) > 0)) {
+                longest = in;
+            }
+        }
+        return longest;
+    }
+
+    private static String findMaximum(Set<String> set) {
+        long max = Long.MIN_VALUE;
+        String key = "";
+
+        for (var in : set) {
+            long number = Long.parseLong(in);
+            if (number > max) {
+                max = number;
+                key = in;
+            }
+        }
+        return key;
     }
 }
